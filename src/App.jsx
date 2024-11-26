70a6e4d6bbd90506e873dffe073a14390f4b71c7
Rjk-Jami <rjkhan.1221@gmail.com>
2024-11-26 12:38:36 +0600

    add cursor layout

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useRef } from "react";
+import "./App.css"; // Include styles here
+import Cursor_layout from "./layouts/cursor_layout/Cursor_layout";
+
+const App = () => {
+  
+
+  return (
+    <>
+      <Cursor_layout></Cursor_layout>
+      
+    </>
+  );
+};
+
+export default App;
---
+++ src/routes/routes.jsx
@@ -0,0 +1,5 @@
+import { Route, Routes } from "react-router";
+
+<Routes>
+<Route path="/" element={<App />} />
+</Routes>
